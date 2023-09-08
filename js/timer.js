156ec0535c8b8192628d21bbda60d8350c26547e
Jake Lee <github@jakelee.co.uk>
2023-09-08 20:41:43 +0100

    Adds displaying series and season info

--- js/timer.js
+++ js/timer.js
@@ -1,13 +1,48 @@
+var firstSeriesStart = new Date(2021, 10, 11, 14, 30, 0);
 var end = getNextThursday();
 var _second = 1000;
 var _minute = _second * 60;
 var _hour = _minute * 60;
 var _day = _hour * 24;
+var seasons = [ 
+    "Wet", 
+    "Storm", 
+    "Dry", 
+    "Hot" 
+];
+var seriesNames = [
+    "Welcome Week",
+    "Welcome to Mexico",
+    "Holiday Special",
+    "Happy New Year",
+    "Horizon World Cup",
+    "Horizon Rush Takeover",
+    "Horizon Customs",
+    "Cinco de Mayo",
+    "German Automotive Excellence",
+    "Hot Wheels",
+    "Extreme E",
+    "Rami's Racing History",
+    "Horizon Road Trip",
+    "Horizon 10-Year Anniversary",
+    "Donut Media",
+    "Horizon Holidays",
+    "#FORDzathon",
+    "Japanese Automotive",
+    "Horizon Wilds Takeover",
+    "Midnights at Horizon",
+    "High Performance",
+    "Explore the Horizon",
+    "Upgrade Heroes",
+    "Summer Party",
+    "Italian Automotive"
+];
 window.onload = pageLoaded;
 
 function pageLoaded() {
     showRemaining();
     setInterval(showRemaining, 1000);
+    getSeriesAndSeason();
 }
 
 // https://stackoverflow.com/a/65869347/608312
@@ -32,3 +67,20 @@ function showRemaining() {
     document.getElementById('countdown').innerHTML += minutes + ' minutes, ';
     document.getElementById('countdown').innerHTML += seconds + ' seconds';
 }
+
+// Reference: https://forza.fandom.com/wiki/Forza_Horizon_5/Festival_Playlist
+function getSeriesAndSeason(date = new Date()) {
+    var totalWeeks = weeksBetween(firstSeriesStart, date);
+    var currentSeries = Math.ceil(totalWeeks / 4);
+    var remainder = (totalWeeks - 1) % 4;
+    document.getElementById('series').innerHTML = currentSeries;
+    document.getElementById('series-name').innerHTML = seriesNames[currentSeries];
+    document.getElementById('season').innerHTML = seasons[remainder];
+    //console.log("Weeks: " + totalWeeks + ". " + date.toString() + ": Series " + currentSeries + " Week " + remainder + " Season " + seasons[remainder]);
+}
+
+// https://stackoverflow.com/a/22859920/608312
+function weeksBetween(d1, d2) {
+    return Math.ceil((d2 - d1) / (7 * 24 * 60 * 60 * 1000));
+}
+   
