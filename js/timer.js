98ffeca102e7e135bc3d3614da06f1a617cee23a
Jake Lee <github@jakelee.co.uk>
2023-09-08 22:09:16 +0100

    Finished up displaying next season and series

--- js/timer.js
+++ js/timer.js
@@ -1,5 +1,6 @@
 var firstSeriesStart = new Date(2021, 10, 11, 14, 30, 0);
-var nextSeasonStart = getNextThursday();
+var nextSeasonStart;
+var nextSeriesStart;
 var _second = 1000;
 var _minute = _second * 60;
 var _hour = _minute * 60;
@@ -41,50 +42,63 @@ var seriesNames = [
 window.onload = pageLoaded;
 
 function pageLoaded() {
-    getSeriesAndSeason();
+    showSeriesAndSeasonInfo();
     showRemaining();
     setInterval(showRemaining, 1000);
 }
 
 // https://stackoverflow.com/a/65869347/608312
-function getNextThursday(d = new Date()) {
+function getNextSeasonStart(d = new Date()) {
     let thursday = new Date(d.getFullYear(), d.getMonth(), d.getDate() + (4 - d.getDay() ), 14, 30, 0, 0);
     thursday <= d? thursday.setDate(thursday.getDate() + 7) : null;
     return thursday;
 }
 
-// https://stackoverflow.com/a/9335296/608312
 function showRemaining() {
     var now = new Date();
-    var distance = nextSeasonStart - now;
-    var days = Math.floor(distance / _day);
-    var hours = Math.floor((distance % _day) / _hour);
-    var minutes = Math.floor((distance % _hour) / _minute);
-    var seconds = Math.floor((distance % _minute) / _second);
+    document.getElementById('next-season-date').innerHTML = nextSeasonStart.toLocaleString();
+    document.getElementById('next-season-countdown').innerHTML = getCountdownText(nextSeasonStart, now);
+    document.getElementById('next-series-date').innerHTML = nextSeriesStart.toLocaleString();
+    document.getElementById('next-series-countdown').innerHTML = getCountdownText(nextSeriesStart, now);
+}
 
-    document.getElementById('next').innerHTML = nextSeasonStart.toLocaleString();
-    document.getElementById('countdown').innerHTML = days + ' days, ';
-    document.getElementById('countdown').innerHTML += hours + ' hours, ';
-    document.getElementById('countdown').innerHTML += minutes + ' minutes, ';
-    document.getElementById('countdown').innerHTML += seconds + ' seconds';
+// https://stackoverflow.com/a/9335296/608312
+function getCountdownText(targetDate, now = Date()) {
+    var difference = targetDate - now;
+    var days = Math.floor(difference / _day);
+    var hours = Math.floor((difference % _day) / _hour);
+    var minutes = Math.floor((difference % _hour) / _minute);
+    var seconds = Math.floor((difference % _minute) / _second);
+    return `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
 }
 
 // Reference: https://forza.fandom.com/wiki/Forza_Horizon_5/Festival_Playlist
-function getSeriesAndSeason(date = new Date()) {
+function showSeriesAndSeasonInfo(date = new Date()) {
+    nextSeasonStart = getNextSeasonStart();
+
     var totalWeeks = weeksBetween(firstSeriesStart, date);
     var currentSeries = Math.ceil(totalWeeks / 4);
     var seasonIndex = (totalWeeks - 1) % 4;
     var nextSeasonIndex = totalWeeks % 4;
-    document.getElementById('series').innerHTML = currentSeries;
-    document.getElementById('series-name').innerHTML = seriesNames[currentSeries] || "Unannounced";
+    var weeksRemainingInSeries = (4 - nextSeasonIndex) % 4;
+    nextSeriesStart = addDays(nextSeasonStart, weeksRemainingInSeries * 7);
+
+    document.getElementById('current-series-number').innerHTML = currentSeries;
+    document.getElementById('current-series-name').innerHTML = seriesNames[currentSeries] || "Unannounced";
     document.getElementById('next-series-name').innerHTML = seriesNames[currentSeries + 1] || "Unannounced";
-    document.getElementById('season').innerHTML = seasons[seasonIndex];
-    document.getElementById('next-season').innerHTML = seasons[nextSeasonIndex];
-    //console.log("Weeks: " + totalWeeks + ". " + date.toString() + ": Series " + currentSeries + " Week " + remainder + " Season " + seasons[remainder]);
+    document.getElementById('current-season-name').innerHTML = seasons[seasonIndex];
+    document.getElementById('next-season-name').innerHTML = seasons[nextSeasonIndex];
 }
 
 // https://stackoverflow.com/a/22859920/608312
 function weeksBetween(d1, d2) {
     return Math.ceil((d2 - d1) / (7 * 24 * 60 * 60 * 1000));
+}
+
+// https://stackoverflow.com/a/19691491/608312
+function addDays(date, days) {
+    var result = new Date(date);
+    result.setDate(result.getDate() + days);
+    return result;
 }
    
