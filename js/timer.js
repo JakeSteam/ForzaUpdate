5dcf2d974dcebe4a8784c5a5388b6a2331e25c64
Jake Lee <github@jakelee.co.uk>
2023-09-08 22:40:29 +0100

    Adds links to series new articles

--- js/timer.js
+++ js/timer.js
@@ -5,39 +5,40 @@ var _second = 1000;
 var _minute = _second * 60;
 var _hour = _minute * 60;
 var _day = _hour * 24;
-var seasons = [ 
-    "Wet", 
-    "Storm", 
-    "Dry", 
-    "Hot" 
+var seasons = [
+    "Wet",
+    "Storm",
+    "Dry",
+    "Hot"
 ];
 var seriesNames = [
-    "Welcome Week",
-    "Welcome to Mexico",
-    "Holiday Special",
-    "Happy New Year",
-    "Horizon World Cup",
-    "Horizon Rush Takeover",
-    "Horizon Customs",
-    "Cinco de Mayo",
-    "German Automotive Excellence",
-    "Hot Wheels",
-    "Extreme E",
-    "Rami's Racing History",
-    "Horizon Road Trip",
-    "Horizon 10-Year Anniversary",
-    "Donut Media",
-    "Horizon Holidays",
-    "#FORDzathon",
-    "Japanese Automotive",
-    "Horizon Wilds Takeover",
-    "Midnights at Horizon",
-    "High Performance",
-    "Explore the Horizon",
-    "Upgrade Heroes",
-    "Summer Party",
-    "Italian Automotive",
-    "Horizon Creatives"
+    { "name": "Welcome Week", "url": "https://forza.net/news/forza-horizon-5-early-access" },
+    { "name": "Welcome to Mexico", "url": "https://forza.net/news/forza-horizon-5-now-available" },
+    { "name": "Holiday Special", "url": "https://forza.net/news/forza-horizon-5-series-2" },
+    { "name": "Happy New Year", "url": "https://forza.net/news/forza-horizon-5-series-3-update" },
+    { "name": "Horizon World Cup", "url": "https://forza.net/news/forza-horizon-5-series-4-update" },
+    { "name": "Horizon Rush Takeover", "url": "https://forza.net/news/forza-horizon-5-series-5-update" },
+    { "name": "Horizon Customs", "url": "https://forza.net/news/forza-horizon-5-series-6-update" },
+    { "name": "Cinco de Mayo", "url": "https://forza.net/news/forza-horizon-5-series-7-update" },
+    { "name": "German Automotive Excellence", "url": "https://forza.net/news/forza-horizon-5-series-8" },
+    { "name": "Hot Wheels", "url": "https://forza.net/news/forza-horizon-5-series-9" },
+    { "name": "Extreme E", "url": "https://forza.net/news/forza-horizon-5-series-10" },
+    { "name": "Rami's Racing History", "url": "https://forza.net/news/forza-horizon-5-ramis-racing-history" },
+    { "name": "Horizon Road Trip", "url": "https://forza.net/news/forza-horizon-5-horizon-road-trip" },
+    { "name": "Horizon 10-Year Anniversary", "url": "https://forza.net/news/forza-horizon-celebrating-a-decade-of-festivals" },
+    { "name": "Donut Media", "url": "https://forza.net/news/forza-horizon-5-donut-media" },
+    { "name": "Horizon Holidays", "url": "https://forza.net/news/forza-horizon-5-holidays" },
+    { "name": "#FORDzathon", "url": "https://forza.net/news/forza-horizon-5-fordzathon" },
+    { "name": "Japanese Automotive", "url": "https://forza.net/news/forza-horizon-5-japanese-automotive" },
+    { "name": "Horizon Wilds Takeover", "url": "https://forza.net/news/forza-horizon-5-horizon-wilds-takeover" },
+    { "name": "Midnights at Horizon", "url": "https://forza.net/news/forza-horizon-5-midnights" },
+    { "name": "High Performance", "url": "https://forza.net/news/forza-horizon-5-high-performance" },
+    { "name": "Explore the Horizon", "url": "https://forza.net/news/forza-horizon-5-explore-the-horizon" },
+    { "name": "Upgrade Heroes", "url": "https://forza.net/news/forza-horizon-5-upgrade-heroes" },
+    { "name": "Summer Party", "url": "https://forza.net/news/forza-horizon-5-summer-party" },
+    { "name": "Italian Automotive", "url": "https://forza.net/news/forza-horizon-5-italian-automotive" },
+    { "name": "Horizon Creatives", "url": "https://forza.net/news/forza-horizon-5-horizon-creatives" },
+    { "name": "(Unconfirmed) Dia de Muertos", "url": "" }
 ];
 window.onload = pageLoaded;
 
@@ -49,8 +50,8 @@ function pageLoaded() {
 
 // https://stackoverflow.com/a/65869347/608312
 function getNextSeasonStart(d = new Date()) {
-    let thursday = new Date(d.getFullYear(), d.getMonth(), d.getDate() + (4 - d.getDay() ), 14, 30, 0, 0);
-    thursday <= d? thursday.setDate(thursday.getDate() + 7) : null;
+    let thursday = new Date(d.getFullYear(), d.getMonth(), d.getDate() + (4 - d.getDay()), 14, 30, 0, 0);
+    thursday <= d ? thursday.setDate(thursday.getDate() + 7) : null;
     return thursday;
 }
 
@@ -84,8 +85,10 @@ function showSeriesAndSeasonInfo(date = new Date()) {
     nextSeriesStart = addDays(nextSeasonStart, weeksRemainingInSeries * 7);
 
     document.getElementById('current-series-number').innerHTML = currentSeries;
-    document.getElementById('current-series-name').innerHTML = seriesNames[currentSeries] || "Unannounced";
-    document.getElementById('next-series-name').innerHTML = seriesNames[currentSeries + 1] || "Unannounced";
+    document.getElementById('current-series-link').innerHTML = seriesNames[currentSeries].name || "Unannounced";
+    document.getElementById('current-series-link').href = seriesNames[currentSeries].url || "https://forza.net/news/";
+    document.getElementById('next-series-link').innerHTML = seriesNames[currentSeries + 1].name || "Unannounced";
+    document.getElementById('next-series-link').href = seriesNames[currentSeries + 1].url || "https://forza.net/news/";
     document.getElementById('current-season-name').innerHTML = seasons[seasonIndex];
     document.getElementById('next-season-name').innerHTML = seasons[nextSeasonIndex];
 }
@@ -101,4 +104,3 @@ function addDays(date, days) {
     result.setDate(result.getDate() + days);
     return result;
 }
-   
