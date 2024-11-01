140b5b40fe065bc50237dfff4b186d28dda71719
Jake Lee <github@jakelee.co.uk>
2024-11-01 20:49:43 +0000

    Update forza.js

--- js/forza.js
+++ js/forza.js
@@ -48,7 +48,8 @@ var seriesNames = [
     { "name": "High-Performance Dailies", "url": "https://forza.net/news/forza-horizon-5-high-performance-dailies" },
     { "name": "Hidden Horizons", "url": "https://forza.net/news/forza-horizon-5-hidden-horizons" },
     { "name": "Back to the 90's", "url": "https://forza.net/news/forza-horizon-5-back-to-the-90s" },
-    { "name": "Horizon Track Day", "url": "https://forza.net/news/" }
+    { "name": "Horizon Track Day", "url": "https://forza.net/news/forza-horizon-5-horizon-track-day" },
+    { "name": "Horizon Holidays Mixup", "url": "https://forza.net/news/" }
 ];
 
 window.addEventListener("load", function() {
