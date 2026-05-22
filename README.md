08ad8848310d50e5a026b10c8cbaf8ddd8f03b4d
Jake Lee <github@jakelee.co.uk>
2026-05-22 09:01:51 +0100

    Update to FH6

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## Info
 
-New seasons are every Thursday at 14:30 GMT, with new challenges & rewards. New series & a large game update drop every 4 weeks.
-      
+New Forza Horizon 6 seasons are every Thursday at 14:30 GMT, with new challenges & rewards. New series & a large game update drop every 4 weeks.
+
 ## Series & seasons
 
 We are currently in Series <span id="current-series-number"></span> (<a id="current-series-link" target="_blank"></a>), <span id="current-season-name"></span> season.
@@ -12,11 +12,9 @@ Next season (<span id="next-season-name"></span>) starts in <b id="next-season-c
 
 ## Links
 
-* [Forza Wiki](https://forza.fandom.com/wiki/Forza_Horizon_5)
-* [Update Release Notes](https://forza.net/news)
-* [Season Information / Tunes (r/Forza)](https://www.reddit.com/r/forza/about/sticky?num=1)
-* [FH5 Badges Guide](https://jakelee.co.uk/complete-guide-to-forza-horizon-5-badges/)
-* [FH5 Creative Accolades Guide](https://jakelee.co.uk/forza-horizon-5-creative-accolades/)
-* [Site Source Code (GitHub)](https://github.com/JakeSteam/ForzaUpdate) & [Announcement Post](https://blog.jakelee.co.uk/forza-update-timer-miniproject/)
+- [Forza Wiki](https://forza.fandom.com/wiki/Forza_Horizon_6)
+- [Update Release Notes](https://forza.net/news)
+- [Season Information / Tunes (r/Forza)](https://www.reddit.com/r/forza/about/sticky?num=1)
+- [Site Source Code (GitHub)](https://github.com/JakeSteam/ForzaUpdate) & [Announcement Post](https://blog.jakelee.co.uk/forza-update-timer-miniproject/)
 
 Got a suggestion / found a bug? [Raise an issue](https://github.com/JakeSteam/ForzaUpdate/issues) or [Email me](mailto:forza@jakelee.co.uk) 😊
--- js/forza.js
+++ js/forza.js
@@ -1,58 +1,15 @@
-var firstSeriesStart = new Date(Date.UTC(2021, 10, 11, 14, 30, 0));
+var firstSeriesStart = new Date(Date.UTC(2026, 5, 22, 14, 30, 0));
 var nextSeasonStart;
 var nextSeriesStart;
 var seasons = [
-    "Summer / Wet",
-    "Autumn / Storm",
-    "Winter / Dry",
-    "Sprint / Hot"
+    "Summer",
+    "Autumn",
+    "Winter",
+    "Sprint"
 ];
 var seriesNames = [
-    { "name": "Welcome Week", "url": "https://forza.net/news/forza-horizon-5-early-access" },
-    { "name": "Welcome to Mexico", "url": "https://forza.net/news/forza-horizon-5-now-available" },
-    { "name": "Holiday Special", "url": "https://forza.net/news/forza-horizon-5-series-2" },
-    { "name": "Happy New Year", "url": "https://forza.net/news/forza-horizon-5-series-3-update" },
-    { "name": "Horizon World Cup", "url": "https://forza.net/news/forza-horizon-5-series-4-update" },
-    { "name": "Horizon Rush Takeover", "url": "https://forza.net/news/forza-horizon-5-series-5-update" },
-    { "name": "Horizon Customs", "url": "https://forza.net/news/forza-horizon-5-series-6-update" },
-    { "name": "Cinco de Mayo", "url": "https://forza.net/news/forza-horizon-5-series-7-update" },
-    { "name": "German Automotive Excellence", "url": "https://forza.net/news/forza-horizon-5-series-8" },
-    { "name": "Hot Wheels", "url": "https://forza.net/news/forza-horizon-5-series-9" },
-    { "name": "Extreme E", "url": "https://forza.net/news/forza-horizon-5-series-10" },
-    { "name": "Rami's Racing History", "url": "https://forza.net/news/forza-horizon-5-ramis-racing-history" },
-    { "name": "Horizon Road Trip", "url": "https://forza.net/news/forza-horizon-5-horizon-road-trip" },
-    { "name": "Horizon 10-Year Anniversary", "url": "https://forza.net/news/forza-horizon-celebrating-a-decade-of-festivals" },
-    { "name": "Donut Media", "url": "https://forza.net/news/forza-horizon-5-donut-media" },
-    { "name": "Horizon Holidays", "url": "https://forza.net/news/forza-horizon-5-holidays" },
-    { "name": "#FORDzathon", "url": "https://forza.net/news/forza-horizon-5-fordzathon" },
-    { "name": "Japanese Automotive", "url": "https://forza.net/news/forza-horizon-5-japanese-automotive" },
-    { "name": "Horizon Wilds Takeover", "url": "https://forza.net/news/forza-horizon-5-horizon-wilds-takeover" },
-    { "name": "Midnights at Horizon", "url": "https://forza.net/news/forza-horizon-5-midnights" },
-    { "name": "High Performance", "url": "https://forza.net/news/forza-horizon-5-high-performance" },
-    { "name": "Explore the Horizon", "url": "https://forza.net/news/forza-horizon-5-explore-the-horizon" },
-    { "name": "Upgrade Heroes", "url": "https://forza.net/news/forza-horizon-5-upgrade-heroes" },
-    { "name": "Summer Party", "url": "https://forza.net/news/forza-horizon-5-summer-party" },
-    { "name": "Italian Automotive", "url": "https://forza.net/news/forza-horizon-5-italian-automotive" },
-    { "name": "Horizon Creatives", "url": "https://forza.net/news/forza-horizon-5-horizon-creatives" },
-    { "name": "Día de Muertos", "url": "https://forza.net/news/forza-horizon-5-dia-de-muertos" },
-    { "name": "American Automotive", "url": "https://forza.net/news/forza-horizon-5-american-automotive" },
-    { "name": "Winter Wonderland", "url": "https://forza.net/news/Winter-Wonderland" },
-    { "name": "Community Choice", "url": "https://forza.net/news/forza-horizon-5-Community-Choice" },
-    { "name": "Lunar New Year", "url": "https://forza.net/news/forza-horizon-5-Lunar-New-Year" },
-    { "name": "European Automotive", "url": "https://forza.net/news/forza-horizon-5-European-Auto" },
-    { "name": "Horizon Race-Off", "url": "https://forza.net/news/forza-horizon-5-Horizon-Race-Off" },
-    { "name": "Apex Allstars", "url": "https://forza.net/news/forza-horizon-5-Apex-AllStars" },
-    { "name": "Horizon Retrowave", "url": "https://forza.net/news/forza-horizon-5-horizon-retrowave" },
-    { "name": "Modern Horizons", "url": "https://forza.net/news/forza-horizon-5-modern-horizons" },
-    { "name": "Horizon Cars and Coffee", "url": "https://forza.net/news/forza-horizon-5-cars-coffee" },
-    { "name": "High-Performance Dailies", "url": "https://forza.net/news/forza-horizon-5-high-performance-dailies" },
-    { "name": "Hidden Horizons", "url": "https://forza.net/news/forza-horizon-5-hidden-horizons" },
-    { "name": "Back to the 90's", "url": "https://forza.net/news/forza-horizon-5-back-to-the-90s" },
-    { "name": "Horizon Track Day", "url": "https://forza.net/news/forza-horizon-5-horizon-track-day" },
-    { "name": "Horizon Holidays Mixup", "url": "https://forza.net/news/forza-horizon-5-horizon-holiday-mixup" },
-    { "name": "Extreme Horizon", "url": "https://forza.net/news/forza-horizon-5-horizon-holiday-mixup" },
-    { "name": "Midnight Muscle", "url": "https://forza.net/news/forza-horizon-5-midnight-muscle" },
-    { "name": "Player Voted (repeat series)", "url": "https://forza.net/news/forza-horizon-5-festival-playlist-voting" }
+    { "name": "Welcome to Japan", "url": "https://forza.net/news/forza-horizon-6-series-1" },
+    { "name": "Horizon Decades", "url": "https://forza.net/" }
 ];
 
 window.addEventListener("load", function() {
@@ -61,7 +18,7 @@ window.addEventListener("load", function() {
     setInterval(updateCountdowns, 1000);
 }, false);
 
-// Reference: https://forza.fandom.com/wiki/Forza_Horizon_5/Festival_Playlist
+// Reference: https://forza.fandom.com/wiki/Forza_Horizon_6/Festival_Playlist
 function showSeriesAndSeasonInfo(date = new Date()) {
     nextSeasonStart = getNextSeasonStart();
 
